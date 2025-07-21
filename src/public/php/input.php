860d7d5f5148b7f36a9d2fa26a229e71d12d2e32
hondaminori <hondaminori.ta3@gmail.com>
2025-07-21 14:18:58 +0900

    37. フォームセキュリティ XSS

--- src/public/php/input.php
+++ src/public/php/input.php
@@ -1,4 +1,7 @@
 <?php
+
+header('X-FRAME-OPTIONS: DENY');
+
 // 0:入力,1:確認,2:完了
 $pageFlag = 0;
 
@@ -6,12 +9,17 @@ if(!empty($_POST['btn_confirm'])) {
   $pageFlag = 1;
 }
 
-if (!empty($_POST['btn_submit'])) {
-  $pageFlag = 2;
-  echo '<pre>';
-  var_dump($_POST);
-  echo '</pre>';
+// if (!empty($_POST['btn_submit'])) {
+//   $pageFlag = 2;
+//   echo '<pre>';
+//   var_dump($_POST);
+//   echo '</pre>';
+// }
+
+function h($str) {
+  return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -23,10 +31,10 @@ if (!empty($_POST['btn_submit'])) {
   <?php if ($pageFlag === 0) : ?>
     <form method="POST" action="input.php">
       氏名
-      <input type="text" name="your_name">
+      <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])) {echo h($_POST['your_name']); } ?>">
       <br>
       メールアドレス
-      <input type="email" name="email">
+      <input type="email" name="email" value="<?php if(!empty($_POST['email'])) {echo h($_POST['email']); } ?>">
       <br>
       <input type="submit" name="btn_confirm" value="確認する">
     </form>
@@ -35,14 +43,15 @@ if (!empty($_POST['btn_submit'])) {
   <?php if ($pageFlag === 1) : ?>
     <form method="POST" action="input.php">
       氏名
-      <?php echo $_POST['your_name']; ?>
+      <?php echo h($_POST['your_name']); ?>
       <br>
       メールアドレス
-      <?php echo $_POST['email']; ?>
+      <?php echo h($_POST['email']); ?>
       <br>
+      <input type="submit" name="back" value="戻る">
       <input type="submit" name="btn_submit" value="送信する">
-      <input type="hidden" name="your_name" value="<?php echo $_POST['your_name']; ?>">
-      <input type="hidden" name="email" value="<?php echo $_POST['email']; ?>">
+      <input type="hidden" name="your_name" value="<?php echo h($_POST['your_name']); ?>">
+      <input type="hidden" name="email" value="<?php echo h($_POST['email']); ?>">
     </form>
   <?php endif; ?>
 
