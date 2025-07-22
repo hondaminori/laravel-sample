12df2df4d79b0fb11035fdc53f05443dc19fec1e
hondaminori <hondaminori.ta3@gmail.com>
2025-07-23 07:41:59 +0900

    38. フォームセキュリティ CFRS

--- src/public/php/input.php
+++ src/public/php/input.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start();
+
 header('X-FRAME-OPTIONS: DENY');
 
 // 0:入力,1:確認,2:完了
@@ -13,9 +15,9 @@ if (!empty($_POST['btn_submit'])) {
   $pageFlag = 2;
 }
 
-//  echo '<pre>';
-//  var_dump($_POST);
-//  echo '</pre>';
+  echo '<pre>';
+  var_dump($_SESSION);
+  echo '</pre>';
 
 function h($str) {
   return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
@@ -30,6 +32,13 @@ function h($str) {
 
 <body>
   <?php if ($pageFlag === 0) : ?>
+  <?php
+    if(!isset($_SESSION['csrfToken'])) {
+      $csrfToken = bin2hex(random_bytes(32));
+      $_SESSION['csrfToken'] = $csrfToken;
+    }
+    $token = $_SESSION['csrfToken'];
+    ?>
     <form method="POST" action="input.php">
       氏名
       <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])) {echo h($_POST['your_name']); } ?>">
@@ -38,26 +47,33 @@ function h($str) {
       <input type="email" name="email" value="<?php if(!empty($_POST['email'])) {echo h($_POST['email']); } ?>">
       <br>
       <input type="submit" name="btn_confirm" value="確認する">
+      <input type="hidden" name="csrf" value="<?php echo $token; ?>">
     </form>
   <?php endif; ?>
 
   <?php if ($pageFlag === 1) : ?>
-    <form method="POST" action="input.php">
-      氏名
-      <?php echo h($_POST['your_name']); ?>
-      <br>
-      メールアドレス
-      <?php echo h($_POST['email']); ?>
-      <br>
-      <input type="submit" name="back" value="戻る">
-      <input type="submit" name="btn_submit" value="送信する">
-      <input type="hidden" name="your_name" value="<?php echo h($_POST['your_name']); ?>">
-      <input type="hidden" name="email" value="<?php echo h($_POST['email']); ?>">
-    </form>
+    <?php if($_POST['csrf'] === $_SESSION['csrfToken']) :?>
+      <form method="POST" action="input.php">
+        氏名
+        <?php echo h($_POST['your_name']); ?>
+        <br>
+        メールアドレス
+        <?php echo h($_POST['email']); ?>
+        <br>
+        <input type="submit" name="back" value="戻る">
+        <input type="submit" name="btn_submit" value="送信する">
+        <input type="hidden" name="your_name" value="<?php echo h($_POST['your_name']); ?>">
+        <input type="hidden" name="email" value="<?php echo h($_POST['email']); ?>">
+        <input type="hidden" name="csrf" value="<?php echo h($_POST['csrf']); ?>">
+      </form>
+    <?php endif; ?>
   <?php endif; ?>
 
   <?php if ($pageFlag === 2) : ?>
-    送信が完了しました。
+    <?php if($_POST['csrf'] === $_SESSION['csrfToken']) :?>
+      送信が完了しました。
+      <?php unset($_SESSION['csrfToken']); ?>
+    <?php endif; ?>
   <?php endif; ?>
 </body>
 
