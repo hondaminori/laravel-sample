b14cc4f9242c5771bbaca5bf3312c197661af505
hondaminori <hondaminori.ta3@gmail.com>
2025-07-24 11:07:34 +0900

    47. ファイル操作 その2　※多数省略

--- src/public/mainte/test.php
+++ src/public/mainte/test.php
@@ -1,4 +1,17 @@
 <?php
-    echo('<br>');
-    echo(password_hash('password123', PASSWORD_BCRYPT));
-?>
+// echo('<br>');
+// echo(password_hash('password123', PASSWORD_BCRYPT));
+
+$contactFile = '.contact.dat';
+
+// ファイル丸ごと読み込み
+$fileContents = file_get_contents($contactFile);
+echo $fileContents;
+
+// ファイルに書き込み(上書き)
+// file_put_contents($contactFile, 'テストです');
+
+$addText = 'テストです' . "\n";
+
+// ファイルに書き込み(追記)
+file_put_contents($contactFile, $addText, FILE_APPEND);
